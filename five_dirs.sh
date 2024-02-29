6f8101d306cf10ab62ab777a032bf9bf4ec32926
victoriaperyniak <peryniak@wisc.edu>
2024-02-29 16:11:45 -0600

    finished five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -2,16 +2,14 @@
 
 mkdir ./five
 
-for num in {1..5..1}
+for num in {1..5..1} #five directories within five/
 do
-    mkdir ./five/dir$num
-    for i in {1..4..1}
-    do
-	touch ./five/dir$num/file$num
-	for idx in {0..$num..1}
+	mkdir ./five/dir$num  # initialize directory
+	for i in {1..4..1} # within that directory, initialize 4 files
 	do
-	    echo $num > ./five/dir$num/file$num
+		touch ./five/dir$num/file$i # initialize file. contents of file[n] = n, repeated n times
+		printf "%0.s$i\n" $(seq $i) >> ./five/dir$num/file$i
+
 	done
-    done
 done
        
