2793e75c2641be61514e034bd71dc6b86ab18631
victoriaperyniak <peryniak@wisc.edu>
2024-02-29 12:01:08 -0600

    five_dirs.sh in progress

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+mkdir ./five
+
+for num in {1..5..1}
+do
+    mkdir ./five/dir$num
+    for i in {1..4..1}
+    do
+	touch file$num
+	for idx in {0..$num..1}
+	do
+	    echo $num > ./five/dir$num/file$num
+	done
+    done
+done
+       
