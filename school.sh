b0575e0b5991038c34c2ada393939386a1c1d78c
Grace Sullivan <gesullivan@wisc.edu>
2024-02-29 12:15:48 -0600

    step 4

--- school.sh
+++ school.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | {
+    value=0
+    num=0
+    while read val; do
+	value=$((value + val))
+	num=$((num + 1))
+    done
+
+    echo "$value / $num" | bc
+}
