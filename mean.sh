c5424db40e627a9d30da2fe419236609f4962cf3
John Oh <oh79@wisc.edu>
2024-03-05 11:52:50 -0600

    usage statement and file reading

--- mean.sh
+++ mean.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+if [$# -lt 1] || [$# -gt 2]; then
+    echo "usage: $0 <column> [file.csv]" 1>&2
+    exit 0
+fi
+
+column=$1
+
+if [ $# -eq 2 ]; then
+    file=$2
+else
+    file="/dev/stdin"
+fi
