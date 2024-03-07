31cc610ee0a0883d1788d046e50fcd5c2887c69a
John Oh <oh79@wisc.edu>
2024-03-07 01:28:35 -0600

    working mean.sh file, check up later for potential mistake

--- mean.sh
+++ mean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [$# -lt 1] || [$# -gt 2]; then
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
     echo "usage: $0 <column> [file.csv]" 1>&2
     exit 0
 fi
@@ -12,3 +12,14 @@ if [ $# -eq 2 ]; then
 else
     file="/dev/stdin"
 fi
+
+cut -d, -f"$column" "$file" | tail -n +2 | {
+    sum=0
+    count=0
+    while read val; do
+	sum=$(($sum + val))
+	count=$(($count + 1))
+    done
+
+    echo "$sum / $count" | bc
+}
